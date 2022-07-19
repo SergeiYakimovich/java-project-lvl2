5771c5c28c2b3e835100957fa0c5eeb69fbee5e1
sergei <s0108@mail.ru>
2022-07-19 19:47:59 +0300

    mapper not work

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,23 +1,46 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import java.io.File;
+import java.nio.file.Path;
+import java.text.Format;
+import java.util.concurrent.Callable;
 
-public class App {
-    public static void main(String[] args) {
+import static java.nio.file.Files.readString;
 
-        int exitCode = new CommandLine(new Tar()).execute(args);
-        System.exit(exitCode);
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<String> {
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private Path filepath1;
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private Path filepath2;
 
-        System.out.println("Hello World!");
+    @CommandLine.Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+    private Format format /*= Format.LONG*/;
+    @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested = false;
+    @CommandLine.Option(names={"-V", "--version"}, description="Print version information and exit.")
+    private boolean verbose;
+
+    @Override
+    public String call() throws Exception {
+        return "";
     }
+    public static void main(String[] args) throws Exception {
 
-    @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
-            description = "Compares two configuration files and shows a difference.")
-    public static class Tar {
-        @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
-        private boolean helpRequested = false;
-        @CommandLine.Option(names={"-V", "--version"}, description="Print version information and exit.")
-        private boolean verbose;
+        App app = new App();
+        int exitCode = new CommandLine(app).execute(args);
+        //System.exit(exitCode);
+
+        String str1 = readString(app.filepath1);
+        System.out.println(str1);
+        String str2 = readString(app.filepath2);
+        System.out.println(str2);
+        System.out.println("OK");
+        String result = Differ.generate(str1, str2);
+        System.out.println(result);
+
 
     }
 
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,10 +1,21 @@
 package hexlet.code;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Differ {
 
-    public static String generate() {
+    public static String generate(String str1, String str2) throws Exception {
 
-        return "";
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map1
+                = mapper.readValue(str1, new TypeReference<Map<String,Object>>(){});
+        Map<String, Object> map2
+                = mapper.readValue(str2, new TypeReference<Map<String,Object>>(){});
+
+        return "123";
     }
 
 }
