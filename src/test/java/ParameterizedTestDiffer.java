a159359e6d5e958d2d97e8d3efe556c3aac58f60
sergei <s0108@mail.ru>
2022-07-27 14:11:53 +0300

    Update

--- src/test/java/ParameterizedTestDiffer.java
+++ src/test/java/ParameterizedTestDiffer.java
@@ -2,36 +2,43 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import static java.nio.file.Files.readString;
 import static org.assertj.core.api.Assertions.assertThat;
+
 public class ParameterizedTestDiffer {
 
     @ParameterizedTest
-    @CsvSource({
-        "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
-                + "src/test/resources/result_J_S.txt, stylish",
-        "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
-                + "src/test/resources/result_J_P.txt, plain",
-        "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
-                + "src/test/resources/result_J_J.txt, json",
-        "src/test/resources/file1_v1.yml, src/test/resources/file2_v1.yml, "
-                + "src/test/resources/result_Y_S.txt, stylish",
-        "src/test/resources/file1_v2.yml, src/test/resources/file2_v2.yml,"
-                + "src/test/resources/result_Y_P.txt, plain",
-        "src/test/resources/file1_v2.yml, src/test/resources/file2_v2.yml,"
-                + "src/test/resources/result_Y_J.txt, json"
-    })
-    public static void testWithCsvSource(String file1, String file2, String resFile, String formatType)
+    @MethodSource
+    void testWithMethodSource(String file1, String file2, String resFile, String formatType)
             throws Exception {
         String result = Differ.generate(file1, file2, formatType);
         String expected = readString(Paths.get(resFile), StandardCharsets.US_ASCII);
         expected = expected.substring(0, expected.length() - 1);
         assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> testWithMethodSource() {
+        return Stream.of(
+                Arguments.of("src/test/resources/file1_v2.json", "src/test/resources/file2_v2.json",
+                        "src/test/resources/result_J_S.txt", "stylish"),
+                Arguments.of("src/test/resources/file1_v2.json", "src/test/resources/file2_v2.json",
+                        "src/test/resources/result_J_P.txt", "plain"),
+                Arguments.of("src/test/resources/file1_v2.json", "src/test/resources/file2_v2.json",
+                        "src/test/resources/result_J_J.txt", "json"),
+                Arguments.of("src/test/resources/file1_v1.yml", "src/test/resources/file2_v1.yml",
+                        "src/test/resources/result_Y_S.txt", "stylish"),
+                Arguments.of("src/test/resources/file1_v2.yml", "src/test/resources/file2_v2.yml",
+                        "src/test/resources/result_Y_P.txt", "plain"),
+                Arguments.of("src/test/resources/file1_v2.yml", "src/test/resources/file2_v2.yml",
+                        "src/test/resources/result_Y_J.txt", "json")
+        );
     }
 
     @Test
