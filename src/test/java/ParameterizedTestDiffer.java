9fc9326527e664cc7c198da1068d3bc3da65f03a
sergei <s0108@mail.ru>
2022-07-27 12:21:02 +0300

    Update

--- src/test/java/ParameterizedTestDiffer.java
+++ src/test/java/ParameterizedTestDiffer.java
@@ -13,20 +13,21 @@ public class ParameterizedTestDiffer {
 
     @ParameterizedTest
     @CsvSource({
-            "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
-                    + "src/test/resources/result_J_S.txt, stylish",
-            "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
-                    + "src/test/resources/result_J_P.txt, plain",
-            "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
-                    + "src/test/resources/result_J_J.txt, json",
-            "src/test/resources/file1_v1.yml, src/test/resources/file2_v1.yml, "
-                    + "src/test/resources/result_Y_S.txt, stylish",
-            "src/test/resources/file1_v2.yml, src/test/resources/file2_v2.yml,"
-                    + "src/test/resources/result_Y_P.txt, plain",
-            "src/test/resources/file1_v2.yml, src/test/resources/file2_v2.yml,"
-                    + "src/test/resources/result_Y_J.txt, json"
+        "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
+                + "src/test/resources/result_J_S.txt, stylish",
+        "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
+                + "src/test/resources/result_J_P.txt, plain",
+        "src/test/resources/file1_v2.json, src/test/resources/file2_v2.json, "
+                + "src/test/resources/result_J_J.txt, json",
+        "src/test/resources/file1_v1.yml, src/test/resources/file2_v1.yml, "
+                + "src/test/resources/result_Y_S.txt, stylish",
+        "src/test/resources/file1_v2.yml, src/test/resources/file2_v2.yml,"
+                + "src/test/resources/result_Y_P.txt, plain",
+        "src/test/resources/file1_v2.yml, src/test/resources/file2_v2.yml,"
+                + "src/test/resources/result_Y_J.txt, json"
     })
-    public static void testWithCsvSource(String file1, String file2, String resFile, String formatType) throws Exception {
+    public static void testWithCsvSource(String file1, String file2, String resFile, String formatType)
+            throws Exception {
         String result = Differ.generate(file1, file2, formatType);
         String expected = readString(Paths.get(resFile), StandardCharsets.US_ASCII);
         expected = expected.substring(0, expected.length() - 1);
